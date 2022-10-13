21959fa442878194ec041dfaaa8d8b0794479d5d
John Doe <SlySpellar@gmail.com>
2022-10-13 06:41:25 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -2,22 +2,5 @@
 #include <string>
 
 int roman(char r) {
-	switch(r) {
-		case 'I':
-			return 1;
-		case 'V':
-			return 5;
-		case 'X':
-			return 10;
-		case 'L':
-			return 50;
-		case 'C':
-			return 100;
-		case 'D':
-			return 500;
-		case 'M':
-			return 1000;
-		default:
-			return 0;
-	}
+	return 0;
 }
